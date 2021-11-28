333b2dcfb84bbced596d0165784ee042b48d6776
xiaoye <3144604@qq.com>
2021-11-28 17:07:23 +0800

    add request

---
+++ db/basemodel.py
@@ -0,0 +1,4 @@
+
+import datetime
+from peewee import *
+
--- request_test/test.py
+++ request_test/test.py
@@ -1,9 +1,27 @@
 import requests
 
-from probe import user_pb2
+# from probe import user_pb2
+#
+# user = user_pb2.Teacher()
+#
+# rsp = requests.get("http://127.0.0.1:8083/someProtoBuf")
+# user.ParseFromString(rsp.content)
+# print(user)
 
-user = user_pb2.Teacher()
 
-rsp = requests.get("http://127.0.0.1:8083/someProtoBuf")
-user.ParseFromString(rsp.content)
-print(user)
+rsp = requests.post("http://127.0.0.1:8083/loginJSON", json={
+    "user": "bo",
+    "password": "123"
+})
+
+
+# rsp = requests.post("http://127.0.0.1:8083/signup", json={
+#     "age": 18,
+#     "name": "xiaoye",
+#     "email": "xiaoasdf@qq.com",
+#     "password": "asdfa",
+#     "re_password": "asdfa",
+# })
+
+
+print(rsp.text)
